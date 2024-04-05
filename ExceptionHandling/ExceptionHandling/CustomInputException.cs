38882e37843dff075df03db0b6049f2f6020a2d5
rares.amza <rares.amza@ROMOB41159>
2024-04-05 10:55:12 +0300

    Exception handling

---
+++ ExceptionHandling/ExceptionHandling/CustomInputException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class CustomInputException : Exception
+    {
+        public CustomInputException() : base() { }
+
+        public CustomInputException(string message) : base(message) { }
+
+        public CustomInputException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExceptionHandling;
+try
+{
+    ValidateInput(10); 
+    ValidateInput(-5); 
+}
+catch (CustomInputException ex)
+{
+    Console.WriteLine("Custom Input Exception: " + ex.Message);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Argument Exception: " + ex.Message);
+    throw; 
+}
+catch (Exception ex)
+{
+    Console.WriteLine("General Exception: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block executed.");
+}
+
+void ValidateInput(int number)
+{
+#if DEBUG
+    Console.WriteLine("Debug mode is enabled.");
+#endif
+
+    if (number < 0)
+    {
+        throw new CustomInputException("Input number cannot be negative.");
+    }
+    else if (number == 0)
+    {
+        throw new ArgumentException("Input number cannot be zero.");
+    }
+    else
+    {
+        Console.WriteLine("Input validation successful.");
+    }
+}
